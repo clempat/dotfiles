d8a7f803079202b829975613f0afb5724db2a6f9
Clément Patout <clement.patout@gmail.com>
2022-06-28 14:20:45 +0200

    feat: add jk back to escape

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -2,16 +2,17 @@
 local M = {}
 
 M.all = {
-  n = {
-    ["<C-p>"] = {":Telescope find_files <CR>", "Find Files with Telescope"},
-    ["<leader>a"] = {":lua require('harpoon.mark').add_file() <CR>", "Add mark to harpoon"},
-    ["<C-e>"] = {":Telescope harpoon marks <CR>", "List marks from harpoon"},
-    ["<leader>xx"] = {"TroubleToggl<CR>", "I forgot"},
-    ["<c-u>"] = {"i<c-r>=trim(system('uuidgen'))<cr><esc>", "Generate uuid when normal mode"},
-  },
-  i = {
-    ["<c-u>"] = {"<c-r>=trim(system('uuidgen'))<cr>", "Generate uuid when insert mode"},
-  }
+	n = {
+		["<C-p>"] = { ":Telescope find_files <CR>", "Find Files with Telescope" },
+		["<leader>a"] = { ":lua require('harpoon.mark').add_file() <CR>", "Add mark to harpoon" },
+		["<C-e>"] = { ":Telescope harpoon marks <CR>", "List marks from harpoon" },
+		["<leader>xx"] = { "TroubleToggl<CR>", "I forgot" },
+		["<c-u>"] = { "i<c-r>=trim(system('uuidgen'))<cr><esc>", "Generate uuid when normal mode" },
+	},
+	i = {
+		["<c-u>"] = { "<c-r>=trim(system('uuidgen'))<cr>", "Generate uuid when insert mode" },
+		["jk"] = { "<ESC>", "Escape with jk" },
+	},
 }
 
 return M
